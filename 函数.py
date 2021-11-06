586ed4d4f04ac6e45acfc56e819422e7464852c6
chendelong277 <chendelong277@163.com>
2021-11-06 18:08:38 +0800

    Update 函数.py

--- 函数.py
+++ 函数.py
@@ -1,5 +1,20 @@
 def say_hello():
     print('hello world')
 
-for i in range(1,5):
+for i in range(1,2):
     say_hello()
+
+def say(message, times=1):
+    print(message*times)
+
+say('hello',5)
+
+def total(a=5,*numbers,**phonebook):
+    print("a",a)
+    for single_item in numbers:
+        print('single_item', single_item)
+
+    for first_part, second_part in phonebook.items():
+            print(first_part,second_part)
+
+print(total(10,1,2,3,Jack=1123,John=2231,Inge=1568))
