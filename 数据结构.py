d4d3ff1898e760f48c28d46043ddc5f594a45c90
chendelong277 <chendelong277@163.com>
2021-11-29 15:36:09 +0800

    Update 函数.py

---
+++ 数据结构.py
@@ -0,0 +1,6 @@
+shoplist=['apple','mango','carrot','banana']
+print('I have', len(shoplist), 'items to purchase')
+
+print('These items are:', end=' ')
+for item in shoplist:
+    print(item, end=' ')
